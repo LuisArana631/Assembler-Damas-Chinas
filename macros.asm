325b75f8f756dd55fc84448c20ccfc127707ea1b
Luis Arana <luarana631@gmail.com>
2020-09-23 11:37:50 -0600

    Print tablero

--- macros.asm
+++ macros.asm
@@ -18,7 +18,7 @@ selectMenu macro option
   je jugar
   cmp al, 32h ;Opcion 2) Cargar Juego
   mov option[0], 32h
-  je cargarJuego
+  je cargar
   cmp al, 33h
   mov option[0], 33h
   je salir
@@ -26,7 +26,355 @@ selectMenu macro option
   jmp inicio
 endm
 
-iniciarJuego macro
-  xor di, di
+imprimirTablero macro arr_fila, fb, fn, ren, reb, fila, libre
+  LOCAL inicio, pintar_espacio, pintar_fb, pintar_fn, pintar_rn, pintar_rb, next, fin
   xor si, si
-  
+  inicio:
+  print fila
+  mov bx, cx
+  mov cx, 8
+  next:
+  mov al, arr_fila[si]
+  cmp al, 000b
+  je pintar_espacio
+  cmp al, 111b
+  je pintar_espacio
+  cmp al, 100b
+  je pintar_fb
+  cmp al, 010b
+  je pintar_fn
+  cmp al, 101b
+  je pintar_rb
+  cmp al, 011b
+  je pintar_rn
+  pintar_espacio:
+  print libre
+  jmp fin
+  pintar_fb:
+  print fb
+  jmp fin
+  pintar_fn:
+  print fn
+  jmp fin
+  pintar_rb:
+  print reb
+  jmp fin
+  pintar_rn:
+  print ren
+  jmp fin
+  fin:
+  inc si
+  cmp si, 8
+  jb next
+endm
+
+get_text_kb macro buffer
+  LOCAL char_get, findout
+  xor si, si
+  char_get:
+  getChar
+  cmp al, 0dh
+  je findout
+  mov buffer[si], al
+  inc si
+  jmp char_get
+  findout:
+  mov al, 24h
+  mov buffer[si], al
+endm
+
+validar_mov macro comando, ficha
+  LOCAL reporte, guardar, movimiento_ficha
+  compare_str comando, cmdExit
+  je inicio
+  compare_str comando, cmdSave
+  je guardar
+  compare_str comando, cmdShow
+  je reporte
+
+  guardar:
+  limpiarBuffer bufferEscritura, SIZEOF bufferEscritura, 24h
+  save_game bufferEscritura, handleFichero, fila8, fila7, fila6, fila5, fila4, fila3, fila2, fila1
+  repetir_turno ficha
+  reporte:
+  print save_
+  movimiento_ficha:
+  print msgMovDia
+  getChar
+endm
+
+repetir_turno macro ficha
+  xor si, si
+  inc si
+  mov al, ficha[si]
+  cmp al, 4eh
+  je turno_negra
+  cmp al, 42h
+  je turno_blanco
+endm
+
+save_game macro buffer, handler, f8, f7, f6, f5, f4, f3, f2 ,f1
+  print name_file
+  get_path buffer
+  crearFile buffer, handler
+  print_ff f8, handler
+  print_ff f7, handler
+  print_ff f6, handler
+  print_ff f5, handler
+  print_ff f4, handler
+  print_ff f3, handler
+  print_ff f2, handler
+  print_ff f1, handler
+  cerrarF handler
+  print salto
+  print save_
+  getChar
+endm
+
+print_ff macro fila, handle
+  LOCAL loop_save, print_clear, print_clearn, print_fb, print_fn, print_rn, print_rb, cond_loop
+  xor si, si
+  loop_save:
+  mov al, fila[si]
+  cmp al, 000b
+  je print_clear
+  cmp al, 111b
+  je print_clearn
+  cmp al, 100b
+  je print_fb
+  cmp al, 010b
+  je print_fn
+  cmp al, 101b
+  je print_rn
+  cmp al, 011b
+  je print_rb
+  print_fb:
+  escribirF SIZEOF FBlanco, FBlanco, handle
+  jmp cond_loop
+  print_fn:
+  escribirF SIZEOF FNegro, FNegro, handle
+  jmp cond_loop
+  print_clear:
+  escribirF SIZEOF VacioP, VacioP, handle
+  jmp cond_loop
+  print_clearn:
+  escribirF SIZEOF VacioNP, VacioNP, handle
+  jmp cond_loop
+  print_rn:
+  escribirF SIZEOF RNegro, RNegro, handle
+  jmp cond_loop
+  print_rb:
+  escribirF SIZEOF RBlanco, RBlanco, handle
+  cond_loop:
+  inc si
+  cmp si, 8
+  jb loop_save
+endm
+
+get_path macro buffer
+  LOCAL get_char, finout
+  xor si, si
+  get_char:
+  getChar
+  cmp al, 0dh
+  je finout
+  mov buffer[si], al
+  inc si
+  jmp get_char
+  finout:
+  mov al, 00h
+  mov buffer[si], al
+endm
+
+compare_str macro str1, str2
+  LOCAL _loop, start_loop, end_loop
+  xor si, si
+  mov cx, 5
+  start_loop:
+  cmp str1[si], 24h
+  je end_loop
+  cmp str2[si], 24h
+  je end_loop
+  mov al, str1[si]
+  mov dl, str2[si]
+  cmp al, dl
+  jne end_loop
+  cmp al, dl
+  je _loop
+  jmp end_loop
+  _loop:
+  inc si
+  dec cx
+  cmp cx, 0001b
+  ja start_loop
+  jmp end_loop
+  end_loop:
+  cmp al, dl
+endm
+
+llenar_filas macro f8, f7, f6, f5, f4, f3, f2, f1, infor, numBytes
+  LOCAL loop_fill, condi_loop, reset_fil, fill_f1, fill_f2, fill_f3, fill_f4, fill_f5, fill_f6, fill_f7, fill_f8
+  xor si, si
+  xor di, di
+
+  loop_fill:
+  cmp si, 8
+  je fill_f8
+  cmp si, 16
+  je fill_f7
+  cmp si, 24
+  je fill_f6
+  cmp si, 32
+  je fill_f5
+  cmp si, 40
+  je fill_f4
+  cmp si, 48
+  je fill_f3
+  cmp si, 56
+  je fill_f2
+  cmp si, 64
+  je fill_f1
+
+  fill_f8:
+  insert_val f8, di, infor[si]
+  jmp condi_loop
+
+  fill_f7:
+  insert_val f7, di, infor[si]
+  jmp condi_loop
+
+  fill_f6:
+  insert_val f6, di, infor[si]
+  jmp condi_loop
+
+  fill_f5:
+  insert_val f5, di, infor[si]
+  jmp condi_loop
+
+  fill_f4:
+  insert_val f4, di, infor[si]
+  jmp condi_loop
+
+  fill_f3:
+  insert_val f3, di, infor[si]
+  jmp condi_loop
+
+  fill_f2:
+  insert_val f2, di, infor[si]
+  jmp condi_loop
+
+  fill_f1:
+  insert_val f1, di, infor[si]
+  jmp condi_loop
+
+  condi_loop:
+  inc si
+  inc di
+  cmp di, 8
+  jb reset_fil
+  xor di, di
+
+  reset_fil:
+  cmp si, 64
+  jb loop_fill
+endm
+
+insert_val macro fila, pos, dato
+LOCAL insert_fn, insert_fb, insert_rn, insert_rb, insert_vp, insert_vnp, end_ins
+  cmp dato, 30h
+  je insert_fn
+  cmp dato, 31h
+  je insert_fb
+  cmp dato, 32h
+  je insert_rn
+  cmp dato, 33h
+  je insert_rb
+  cmp dato, 34h
+  je insert_vp
+  cmp dato, 35h
+  je insert_vnp
+
+  insert_fn:
+    mov al, 010b
+    mov fila[pos], al
+    jmp end_ins
+
+  insert_fb:
+    mov al, 100b
+    mov fila[pos], al
+    jmp end_ins
+
+  insert_rn:
+    mov al, 011b
+    mov fila[pos], al
+    jmp end_ins
+
+  insert_rb:
+    mov al, 101b
+    mov fila[pos], al
+    jmp end_ins
+
+  insert_vp:
+    mov al, 000b
+    mov fila[pos], al
+    jmp end_ins
+
+  insert_vnp:
+    mov al, 111b
+    mov fila[pos], al
+    jmp end_ins
+
+  end_ins:
+
+
+endm
+
+clear_screen macro
+  mov ax, 03h
+  int 10h
+endm
+
+cerrarF macro handle
+  mov ah, 3eh
+  mov bx, handle
+  int 21h
+  jc errorCerrar
+endm
+
+abrirF macro ruta, handle
+  mov ah, 3dh
+  mov al, 02h
+  lea dx, ruta
+  int 21h
+  mov handle, ax
+  jc errorAbrir
+endm
+
+leerF macro numBytes, buffer, handle
+  mov ah, 3fh
+  mov bx, handle
+  mov cx, numBytes
+  lea dx, buffer
+  int 21h
+  jc errorLeer
+endm
+
+escribirF macro numBytes, buffer, handle
+  mov ah, 40h
+  mov bx, handle
+  mov cx, numBytes
+  lea dx, buffer
+  int 21h
+  jc errorEscribir
+endm
+
+limpiarBuffer macro buffer, numBytes, char
+  LOCAL loop_
+  xor si, si
+  xor cx, cx
+  mov cx, numBytes
+  loop_:
+  mov buffer[si], char
+  inc si
+  loop loop_
+endm
--- main.asm
+++ main.asm
@@ -5,13 +5,92 @@ crearFile macro ruta, handle
     mov cx, 00h
     lea dx, ruta
     int 21h
-    jc ErrorCrear
+    jc errorCrear
     mov handle, ax
+endm
+
+crearTablero macro
+  print separador
+  print salto
+  imprimirTablero fila8, fb, fn, ren, reb, f8, vacio
+  print separador
+  print salto
+  imprimirTablero fila7, fb, fn, ren, reb, f7, vacio
+  print separador
+  print salto
+  imprimirTablero fila6, fb, fn, ren, reb, f6, vacio
+  print separador
+  print salto
+  imprimirTablero fila5, fb, fn, ren, reb, f5, vacio
+  print separador
+  print salto
+  imprimirTablero fila4, fb, fn, ren, reb, f4, vacio
+  print separador
+  print salto
+  imprimirTablero fila3, fb, fn, ren, reb, f3, vacio
+  print separador
+  print salto
+  imprimirTablero fila2, fb, fn, ren, reb, f2, vacio
+  print separador
+  print salto
+  imprimirTablero fila1, fb, fn, ren, reb, f1, vacio
+  print separador
+  print cols
+  print salto
+  print salto
+endm
+
+iniciarJuego macro
+  iniciargame:
+  mov puntosB, 0
+  mov puntosN, 0
+  clear_screen
+  print key_start
+  print salto
+  crearTablero
+  getChar
+
+  turno_blanco:
+  limpiarBuffer str_instr, SIZEOF str_instr, 24h
+  clear_screen
+  print turnoB
+  get_text_kb str_instr
+  validar_mov str_instr, fb
+  crearTablero
+  getChar
+
+  turno_negra:
+  limpiarBuffer str_instr, SIZEOF str_instr, 24h
+  clear_screen
+  print turnoN
+  get_text_kb str_instr
+  validar_mov str_instr, fn
+  crearTablero
+  getChar
+
+  jmp turno_blanco
+endm
+
+cargarJuego macro
+  clear_screen
+  limpiarBuffer bufferLectura, SIZEOF bufferLectura, 24h
+  print pathFile
+  get_text_kb bufferLectura
+  print bufferLectura
+  abrirF bufferLectura, handleAux
+  loop_read:
+  limpiarBuffer bufferAuxiliar, SIZEOF bufferAuxiliar, 24h
+  leerF SIZEOF bufferAuxiliar, bufferAuxiliar, handleAux
+  llenar_filas fila8, fila7, fila6, fila5, fila4, fila3, fila2, fila1, bufferAuxiliar, SIZEOF bufferAuxiliar
+  jmp jugar
 endm
 
 .model small
 .stack
 .data
+
+  puntosN WORD 0
+  puntosB WORD 0
 
   encabezado0 db 0ah, 0dh, " ______________________________________________________", "$"
   encabezado1 db 0ah, 0dh, "|                                                      |", "$"
@@ -31,21 +110,93 @@ endm
   opcion5     db 0ah, 0dh, "|______________________________________________________|", "$"
 
   textoOpcion db 0ah, 0dh, "  Seleccione una Opcion: ", "$"
+  turnoB      db 0ah, 0dh, "  Turno Blancas: ", "$"
+  turnoN      db 0ah, 0dh, "  Turno Negras: ", "$"
+  pathFile    db 0ah, 0dh, "  Ingrese la ruta del archivo: ", "$"
+  key_start   db 0ah, 0dh, "  Presiona una tecla para iniciar.", "$"
+  name_file   db 0ah, 0dh, "  Ingresa el nombre del archivo: ", "$"
+  save_       db 0ah, 0dh, "  -> Juego guardado exitosamente!", "$"
+  load_       db 0ah, 0dh, "  -> Juego cargado exitosamente!", "$"
 
-  msgError0    db 0ah, 0dh, "  ** Error al crear archivo **", "$"
+  ;Contenido de tablero
+  f8 db " 8  |", "$"
+  f7 db " 7  |", "$"
+  f6 db " 6  |", "$"
+  f5 db " 5  |", "$"
+  f4 db " 4  |", "$"
+  f3 db " 3  |", "$"
+  f2 db " 2  |", "$"
+  f1 db " 1  |", "$"
 
+  fn    db "FN|", "$"
+  fb    db "FB|", "$"
+  ren   db "RN|", "$"
+  reb   db "RB|", "$"
+  vacio db "  |", "$"
+
+  separador db 0ah, 0dh, "    -------------------------", "$"
+  cols      db 0ah, 0dh, "     A  B  C  D  E  F  G  H ", " $"
+
+  msgError0   db 0ah, 0dh, "  ** Error al crear archivo **", "$"
+  msgError1   db 0ah, 0dh, "  ** Error al abrir archivo **", "$"
+  msgError2   db 0ah, 0dh, "  ** Error al leer archivo **", "$"
+  msgError3   db 0ah, 0dh, "  ** Error al escribir archivo **", "$"
+  msgError4   db 0ah, 0dh, "  ** Error al cerrar archivo **", "$"
+  msgMovIn    db 0ah, 0dh, "  ** No se puede retroceder la ficha **", "$"
+  msgMovDia   db 0ah, 0dh, "  ** Solo se puede mover en diagonal **", "$"
+
+  str_instr db 4 dup('$')
+  ;Archivos
   bufferLectura db 1000 dup('$')
   bufferEscritura db 1000 dup('$')
   bufferAuxiliar db  1000 dup('$')
+  handleFichero dw ?
+  handleAux dw ?
+
 
   opcionActual db ' ', '$'
+  turno db 0b
+
+  ;espacio correcto 000
+  ;espacio incorrecto 111
+
+  ;ficha blanca 100
+  ;ficha negra 010
+  ;reina blanca 101
+  ;reina negra 011
+
+  fila8 db 100b, 111b, 100b, 111b, 100b, 111b, 100b, 111b
+  fila7 db 111b, 100b, 111b, 100b, 111b, 100b, 111b, 100b
+  fila6 db 100b, 111b, 100b, 111b, 100b, 111b, 100b, 111b
+  fila5 db 111b, 000b, 111b, 000b, 111b, 000b, 111b, 000b
+  fila4 db 000b, 111b, 000b, 111b, 000b, 111b, 000b, 111b
+  fila3 db 111b, 010b, 111b, 010b, 111b, 010b, 111b, 010b
+  fila2 db 010b, 111b, 010b, 111b, 010b, 111b, 010b, 111b
+  fila1 db 111b, 010b, 111b, 010b, 111b, 010b, 111b, 010b
 
   salto db 0ah, 0dh, "$"
+
+  cmdExit db "EXIT"
+  cmdSave db "SAVE"
+  cmdShow db "SHOW"
+
+  numbers db 4 dup('$'), '$'
+  arr_aux db 8 dup('$'), '$'
+
+  FNegro db "0"
+  FBlanco db "1"
+  RNegro db "2"
+  RBlanco db "3"
+  VacioP db "4"
+  VacioNP db "5"
+
+  TIME db  '00:00:00', '$'
 
 .code
 
 main proc
   inicio:
+    clear_screen
     ;-------------Mostrar encabezado
     print encabezado0
     print encabezado1
@@ -70,19 +221,65 @@ main proc
     print salto
     print salto
   jugar:
-    print opcion2
+    iniciarJuego
     jmp inicio
-  cargarJuego:
-    print opcion3
+  cargar:
+    cargarJuego
+    getChar
     jmp inicio
   errorCrear:
     print msgError0
+    getChar
+    jmp inicio
+  errorAbrir:
+    print msgError1
+    getChar
+    jmp inicio
+  errorLeer:
+    print msgError2
+    getChar
+    jmp inicio
+  errorEscribir:
+    print msgError3
+    getChar
+    jmp inicio
+  errorCerrar:
+    print msgError4
     getChar
     jmp inicio
   salir:
     mov ah, 4ch
     xor al, al
     int 21h
+
+  get_time proc
+    push ax
+    push cx
+    mov ah, 2ch
+    int 21h
+    mov al, ch
+    call convert
+    mov [bx], ax
+    mov al, cl
+    call convert
+    mov [bx+3], ax
+    mov al, dh
+    call convert
+    mov [bx+6], ax
+    pop cx
+    pop ax
+    ret
+  get_time endp
+
+  convert proc
+    push dx
+    mov ah, 0
+    mov dl, 10
+    div dl
+    or ax, 3030h
+    pop dx
+    ret
+  convert endp
 
 main endp
 end main
